8ec97c8d6e7432e74b33cc6827169d18881143e9
Bilal <ranabilalrajput09@gmail.com>
2023-08-02 12:34:37 +0200

    Fix linters

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  
-  describe "GET /users" do
-    it "Should back with correct response" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    it 'Should back with correct response' do
       get users_path
       expect(response).to have_http_status(200)
     end
 
-     it "Should render index view" do
+    it 'Should render index view' do
       get users_path
-      expect(response).to render_template("users/index")
-      expect(response.body).to include("users_index")
+      expect(response).to render_template('users/index')
+      expect(response.body).to include('users_index')
     end
 
-    it "Should render show view" do
-      get "/users/2"
-      expect(response).to render_template("users/show")
-      expect(response.body).to include("users_view")
+    it 'Should render show view' do
+      get '/users/2'
+      expect(response).to render_template('users/show')
+      expect(response.body).to include('users_view')
     end
   end
 end
