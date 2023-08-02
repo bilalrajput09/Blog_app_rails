d5c848d06b0e6337ca62594f2d27e8033dea4b0a
Bilal <ranabilalrajput09@gmail.com>
2023-08-02 10:58:49 +0200

    Fix issues with assosiations

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,17 +5,17 @@ class Post < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  belongs_to :author, foreign_key: :author_id, class_name: 'User'
+  belongs_to :user, foreign_key: :author_id, class_name: 'User'
 
   after_create :increment_posts_counter
   after_destroy :decrement_posts_counter
 
   def increment_posts_counter
-    author.increment!(:posts_counter)
+    user.increment!(:posts_counter)
   end
 
   def decrement_posts_counter
-    author.decrement!(:posts_counter)
+    user.decrement!(:posts_counter)
   end
 
   def return_5_recent_comments
