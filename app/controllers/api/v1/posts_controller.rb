a2321fb6822c27dd2f7b54d592a90db1f52bb7ba
Ashna Ali <ashnaalipk@gmail.com>
2023-08-10 23:03:31 +0500

    Update posts_controller.rb

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,14 @@
 module Api
     module V1
         class PostsController < ApplicationController
-            
+            def index 
+                user = User.find(params[:user_id])
+                posts = user.posts 
+                render json: posts
+
+            rescue StandardError
+                render json: { error: 'User not found' }, status: :bad_request
+end
         end
     end
 end
