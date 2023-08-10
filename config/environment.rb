93a3666d56629c2cbef59c30fe971c05af7bc2a7
Bilal <ranabilalrajput09@gmail.com>
2023-08-10 11:22:20 +0200

    Fix the issues

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.perform_deliveries = true
-# ActionMailer::Base.smtp_settings = {
-#     address: 465,
-#     domain: "gmail.com",
-#     user_name: ENV['sportscloths09@gmail.com'],
-#     password: ENV['9266qw4hD@'],
-#     authentication: 'plain',
-#     :ssl => true,
-#     :tsl => true,
-#     enable_starttls_auto: true
-# }
